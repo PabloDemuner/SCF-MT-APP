9f75f6a32a4a144c06259274c6b1f7d76d00cdd1
Pablo <pablodiegodemuner@gmail.com>
2021-08-12 01:00:15 -0400

    FEAT: Configuração de fontes e design da tela de lançamentos

--- sistema-controle-financeiro-app/src/app/app.module.ts
+++ sistema-controle-financeiro-app/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TabViewModule } from 'primeng/tabview';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    TabViewModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
