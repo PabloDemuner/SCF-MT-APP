3bd2033cb03208ae914313cf193d2548d9ea3619
Pablo <pablodiegodemuner@gmail.com>
2021-08-16 09:29:05 -0400

    FIX: Removido tela de pesquisa de lancamentos do componente principal.

--- sistema-controle-financeiro-app/src/app/app.component.spec.ts
+++ sistema-controle-financeiro-app/src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'sistema-controle-financeiro-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sistema-controle-financeiro-app');
+    expect(app).toEqual('sistema-controle-financeiro-app');
   });
 
   it('should render title', () => {
