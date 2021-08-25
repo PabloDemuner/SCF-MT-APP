dc4c6b35402e16be0ce440174a9d2d2811b71cb0
Pablo <pablodiegodemuner@gmail.com>
2021-08-24 23:36:14 -0400

    FEAT: Adicionado Mock temporario para testes dos campos de tipos de despesas, Categorias e Pessoa

--- sistema-controle-financeiro-app/src/app/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ sistema-controle-financeiro-app/src/app/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -7,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LancamentosCadastroComponent implements OnInit {
 
+  tipos = [
+    {label: 'Receita', value: 'RECEITA'},
+    {label: 'Despesa', value: 'DESPESA'}
+  ];
+
+  categorias = [
+    {label: 'Alimentação', value: '1'},
+    {label: 'Transporte', value: '2'}
+  ];
+
+  pessoas = [
+    {label: 'João de Souza', value: '3'},
+    {label: 'Maria da Silva', value: '4'},
+    {label: 'Pablo Demuner', value: '5'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
