23c4479a45f2cc527fc3d83abeb4e155b5d464d4
Pablo <pablodiegodemuner@gmail.com>
2021-08-24 23:38:47 -0400

    FEAT: Adicionado os modulos de Calendar,Dropdown e SelectButton

--- sistema-controle-financeiro-app/src/app/app.module.ts
+++ sistema-controle-financeiro-app/src/app/app.module.ts
@@ -1,17 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
 import { TabViewModule } from 'primeng/tabview';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import {TooltipModule} from 'primeng/tooltip';
+import { BrowserModule } from '@angular/platform-browser';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 @NgModule({
   declarations: [
@@ -29,6 +33,10 @@ import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos
     TableModule,
     TooltipModule,
     InputTextareaModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
