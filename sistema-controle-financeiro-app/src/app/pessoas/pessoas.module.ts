06337d1d19565ddab7f09ae422938ee808531579
Pablo <pablodiegodemuner@gmail.com>
2021-09-07 09:22:52 -0400

    FEAT: Imnplementado o modulo de pessoas

---
+++ sistema-controle-financeiro-app/src/app/pessoas/pessoas.module.ts
@@ -0,0 +1,54 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { TooltipModule } from 'primeng/tooltip';
+import { CalendarModule } from 'primeng/calendar';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
+
+import { SharedModule } from './../shared/shared.module';
+
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxCurrencyModule } from 'ngx-currency';
+
+@NgModule({
+  declarations: [
+    PessoaCadastroComponent,
+    PessoaPesquisaComponent,
+    PessoasGridComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+
+    SharedModule,
+
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule,
+    InputTextareaModule,
+    BrowserAnimationsModule,
+    CalendarModule,
+    SelectButtonModule,
+    DropdownModule,
+    NgxCurrencyModule,
+    InputMaskModule
+  ],
+
+  exports: [
+    PessoaCadastroComponent,
+    PessoaPesquisaComponent,
+  ]
+})
+export class PessoasModule { }
