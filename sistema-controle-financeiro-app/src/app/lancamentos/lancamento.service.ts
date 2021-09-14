54e2688c806d5da1230869ad01397ca05ae84ef0
Pablo <pablodiegodemuner@gmail.com>
2021-09-13 23:13:02 -0400

    FEAT: Implementado metodo a Service de lancamentos para fazer consumo dos dados direto da API

---
+++ sistema-controle-financeiro-app/src/app/lancamentos/lancamento.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import * as moment from 'moment';
+
+export class LancamentoFiltro {
+  descricao: string;
+  dataVencimentoInicio: Date;
+  dataVencimentoFim: Date;
+  pagina = 0;
+  itensPorPagina = 5;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LancamentoService {
+
+  lancamentosUrl = 'http://localhost:8080/lancamentos';
+
+  constructor(private http: HttpClient) { }
+
+  pesquisar(filtro: LancamentoFiltro): Promise<any> {
+
+    let params = new HttpParams();
+    const headers = new HttpHeaders()
+      .append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzE1ODg5NzAsInVzZXJfbmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsImF1dGhvcml0aWVzIjpbIlJPTEVfQ0FEQVNUUkFSX0NBVEVHT1JJQSIsIlJPTEVfUEVTUVVJU0FSX1BFU1NPQSIsIlJPTEVfUkVNT1ZFUl9QRVNTT0EiLCJST0xFX0NBREFTVFJBUl9MQU5DQU1FTlRPIiwiUk9MRV9QRVNRVUlTQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUkVNT1ZFUl9MQU5DQU1FTlRPIiwiUk9MRV9DQURBU1RSQVJfUEVTU09BIiwiUk9MRV9QRVNRVUlTQVJfQ0FURUdPUklBIl0sImp0aSI6IjRhOWIwOTAxLTIwODAtNGZhZi04OTU4LWQxMzQ5ZmIyYmJjMyIsImNsaWVudF9pZCI6ImFuZ3VsYXIiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.cOBMmC3jD4AXxrwjP_v4ERLqGFL1ZeW1c_cpPTdfvOA');
+
+    params = params.set('page', filtro.pagina.toString());
+    params = params.set('size', filtro.itensPorPagina.toString());
+
+    if (filtro.descricao) {
+      params = params.set('descricao', filtro.descricao);
+    }
+
+    if (filtro.dataVencimentoInicio) {
+      params = params.set('dataVencimentoApartir', moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'));
+    }
+
+    if (filtro.dataVencimentoFim) {
+      params = params.set('dataVencimentoAte', moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'));
+    }
+
+    return this.http.get(`${this.lancamentosUrl}?resumo`, { headers, params })
+      .toPromise()
+      .then(response => {
+        const lancamentos = response['content'];
+
+        const resultado = {
+          lancamentos: lancamentos,
+          total: response['totalElements']
+        };
+        return resultado;
+      });
+  }
+}
