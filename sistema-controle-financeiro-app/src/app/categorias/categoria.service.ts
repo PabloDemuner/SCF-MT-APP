62b7214c19549f323cfbde89484fad8aca0682a4
Pablo <pablodiegodemuner@gmail.com>
2021-09-27 23:56:26 -0400

    FEAT: Adicionado servico de categorias e implementado o metodo listar

---
+++ sistema-controle-financeiro-app/src/app/categorias/categoria.service.ts
@@ -0,0 +1,21 @@
+import { ICategoria } from './model/categoria.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  categoriasUrl = 'http://localhost:8080/categorias';
+
+  constructor(private http: HttpClient) { }
+
+  listar(): Observable<ICategoria[]> {
+    const headers = new HttpHeaders()
+    .append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MzI4MDA2MDksInVzZXJfbmFtZSI6ImFkbWluQGdtYWlsLmNvbSIsImF1dGhvcml0aWVzIjpbIlJPTEVfQ0FEQVNUUkFSX0NBVEVHT1JJQSIsIlJPTEVfUEVTUVVJU0FSX1BFU1NPQSIsIlJPTEVfUkVNT1ZFUl9QRVNTT0EiLCJST0xFX0NBREFTVFJBUl9MQU5DQU1FTlRPIiwiUk9MRV9QRVNRVUlTQVJfTEFOQ0FNRU5UTyIsIlJPTEVfUkVNT1ZFUl9MQU5DQU1FTlRPIiwiUk9MRV9DQURBU1RSQVJfUEVTU09BIiwiUk9MRV9QRVNRVUlTQVJfQ0FURUdPUklBIl0sImp0aSI6ImQyZDllYmM4LTJkMjQtNGUwOC1iZmRhLWJlYTA0NDM1YjQ2ZCIsImNsaWVudF9pZCI6ImFuZ3VsYXIiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.yx_OlZ7Y7X9YfkzMvBoLFH-NybhrmxDJi6mfKLAYXDA');
+
+    return this.http.get<ICategoria[]>(this.categoriasUrl, { headers });
+  }
+}
