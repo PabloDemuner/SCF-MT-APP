ce4334cdd398ee0ae89c3245061d9c258a9eae29
Pablo <pablodiegodemuner@gmail.com>
2021-09-27 23:51:50 -0400

    FEAT: Retirado o Mock e implementado os metodos de busca da API as "Categorias" e "Pessoas" na pagina de Cadastro de Lancamentos

--- sistema-controle-financeiro-app/src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ sistema-controle-financeiro-app/src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -1,4 +1,10 @@
+import { Lancamento } from './../model/lamcamento-impl.model';
+import { ILancamento } from './../model/lancamento.model';
+import { CategoriaService } from './../../categorias/categoria.service';
 import { Component, OnInit } from '@angular/core';
+import { ErrorHandlerService } from 'src/app/core/error-handler.service';
+import { PessoaService } from 'src/app/pessoas/pessoa.service';
+import { IPessoa } from 'src/app/pessoas/model/pessoa.model';
 
 @Component({
   selector: 'app-lancamentos-cadastro',
@@ -12,20 +18,40 @@ export class LancamentosCadastroComponent implements OnInit {
     {label: 'Despesa', value: 'DESPESA'}
   ];
 
-  categorias = [
-    {label: 'Alimentação', value: '1'},
-    {label: 'Transporte', value: '2'}
-  ];
+  categorias = [];
 
-  pessoas = [
-    {label: 'João de Souza', value: '3'},
-    {label: 'Maria da Silva', value: '4'},
-    {label: 'Pablo Demuner', value: '5'}
-  ];
+  pessoas = [];
 
-  constructor() { }
+  lancamento: ILancamento = new Lancamento();
 
-  ngOnInit(): void {
+  constructor(
+    private pessoaService: PessoaService,
+    private categoriaService: CategoriaService,
+    private errorHandler: ErrorHandlerService
+  ) { }
+
+  ngOnInit() {
+    this.carregaCategorias()
+    this.carregaPessoas()
   }
 
+  carregaCategorias() {
+    return this.categoriaService.listar()
+    .subscribe(categorias => {
+      this.categorias = categorias.map(categoria => {
+        return {label: categoria.nome, value: categoria.id};
+      });
+    }),
+    (error => this.errorHandler.handle(error));
+  }
+
+  carregaPessoas() {
+    return this.pessoaService.listarTodas()
+    .subscribe(pessoas => {
+      this.pessoas = pessoas.content.map((pessoa: IPessoa) => {
+        return {label: pessoa.nome, value: pessoa.id};
+      });
+    }),
+    (error => this.errorHandler.handle(error));
+  }
 }
