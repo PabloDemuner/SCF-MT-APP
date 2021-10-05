337453910a7b4fd0277f59bf26566e55244601eb
Pablo <pablodiegodemuner@gmail.com>
2021-10-05 16:22:09 -0400

    FIX: Alteracao nos metodos carregarPessoas e carregarCategorias e feito implementacao do metodo de salvar um lancamento

--- sistema-controle-financeiro-app/src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
+++ sistema-controle-financeiro-app/src/app/lancamentos/lancamentos-cadastro/lancamentos-cadastro.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
 import { ErrorHandlerService } from 'src/app/core/error-handler.service';
 import { PessoaService } from 'src/app/pessoas/pessoa.service';
 import { IPessoa } from 'src/app/pessoas/model/pessoa.model';
+import { NgForm } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { LancamentoService } from '../lancamento.service';
 
 @Component({
   selector: 'app-lancamentos-cadastro',
@@ -14,8 +17,8 @@ import { IPessoa } from 'src/app/pessoas/model/pessoa.model';
 export class LancamentosCadastroComponent implements OnInit {
 
   tipos = [
-    {label: 'Receita', value: 'RECEITA'},
-    {label: 'Despesa', value: 'DESPESA'}
+    { label: 'Receita', value: 'RECEITA' },
+    { label: 'Despesa', value: 'DESPESA' }
   ];
 
   categorias = [];
@@ -27,7 +30,9 @@ export class LancamentosCadastroComponent implements OnInit {
   constructor(
     private pessoaService: PessoaService,
     private categoriaService: CategoriaService,
-    private errorHandler: ErrorHandlerService
+    private errorHandler: ErrorHandlerService,
+    private messageService: MessageService,
+    private lancamentoService: LancamentoService
   ) { }
 
   ngOnInit() {
@@ -37,21 +42,32 @@ export class LancamentosCadastroComponent implements OnInit {
 
   carregaCategorias() {
     return this.categoriaService.listar()
-    .subscribe(categorias => {
-      this.categorias = categorias.map(categoria => {
-        return {label: categoria.nome, value: categoria.id};
-      });
-    }),
-    (error => this.errorHandler.handle(error));
+      .subscribe(categorias => {
+        this.categorias = categorias.map(categoria => {
+          return { label: categoria.nome, value: categoria.id };
+        });
+      }),
+      (error => this.errorHandler.handle(error));
   }
 
   carregaPessoas() {
     return this.pessoaService.listarTodas()
-    .subscribe(pessoas => {
-      this.pessoas = pessoas.content.map((pessoa: IPessoa) => {
-        return {label: pessoa.nome, value: pessoa.id};
-      });
-    }),
-    (error => this.errorHandler.handle(error));
+      .subscribe(pessoas => {
+        this.pessoas = pessoas.content.map((pessoa: IPessoa) => {
+          return { label: pessoa.nome, value: pessoa.id };
+        });
+      }),
+      (error => this.errorHandler.handle(error));
+  }
+
+  salvar(lancamentoForm: NgForm) {
+    this.lancamentoService.adicionar(this.lancamento)
+      .subscribe(() => {
+        this.messageService.add({ severity: 'success', detail: 'Lancamento adicionado com sucesso!' });
+        lancamentoForm.reset();
+        this.lancamento = new Lancamento();
+      },
+        error => this.errorHandler.handle(error)
+      );
   }
 }
