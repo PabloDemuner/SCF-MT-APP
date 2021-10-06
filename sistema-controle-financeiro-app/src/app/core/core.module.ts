75558f963c4b6ce0ade496fe4cfb1cfb35c9c3cc
Pablo <pablodiegodemuner@gmail.com>
2021-10-05 21:36:39 -0400

    FEAT: Adicionado RouterModule para navegacao entre as paginas no menu

--- sistema-controle-financeiro-app/src/app/core/core.module.ts
+++ sistema-controle-financeiro-app/src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { ErrorHandlerService } from './error-handler.service';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
@@ -10,13 +11,13 @@ import { ConfirmationService, MessageService } from 'primeng/api';
 
 import { NavbarComponent } from './navbar/navbar.component';
 
-
 @NgModule({
   declarations: [
     NavbarComponent
   ],
   imports: [
     CommonModule,
+    RouterModule,
 
     ToastModule,
     ConfirmDialogModule,
